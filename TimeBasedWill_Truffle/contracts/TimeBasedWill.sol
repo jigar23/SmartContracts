70652c297acc2810ce4dc8413de1f7a59b04eaca
Jigar Gada <jigargada90@gmail.com>
2018-08-08 09:20:55 -0700

    Adding split payment functionality

--- TimeBasedWill_Truffle/contracts/TimeBasedWill.sol
+++ TimeBasedWill_Truffle/contracts/TimeBasedWill.sol
@@ -7,12 +7,11 @@ import "./SafeMath.sol";
  * @title TimeBasedWill
  * @dev TimeBasedWill provides ether transfer functionalities to owner's
  *  beneficiaries based on a timer.
- *  - Owner can set the timer(in seconds) to approximately the time they think they'll die
- *      They can change the expiry duration any time.
- *  - Beneficiaries can be added/removed before the expiry time.
+ *  - Owner can set the timer(in seconds) to approximately the time they think they want to transfer
+ *      They can change the expiry duration any time before the last expiry time expires
+ *  - Owner can Add beneficiaries with percent shares for individual beneficiaries
  *  - Funds can be added/removed from the contract before the expiry time ONLY by the owner.
- *  - Once the timer is up, anyone can call the function claimOwnership() and the funds
- *      will be transferred to the beneficiaries.
+ *  - Once the timer is up, beneficiaries can claim their ownership ONCE.
  *  - There are 2 additional functionalities which should be used VERY CAREFULLY
  *      TranferOwnership - This will transfer the onwership of the contract to the new owner.
  *          New Owner will now be able to control all funds in the contract.
@@ -23,9 +22,18 @@ contract TimeBasedWill is Ownable {
 
     using AddressArrayExtended for address[];
     using SafeMath for uint;
-    address[] private m_beneficiaries;
-    uint private m_expiryTime;
-    event benefeciary_address(address benefeciary, uint balance);
+    using SafeMath8 for uint8;
+
+    event transferred_value(address benefeciary, uint balance);
+
+    struct Share {
+        uint8 m_percent_share;
+        bool m_claimed_ownership;
+    }
+
+    address[] public m_beneficiaries;
+    mapping(address => Share) public m_beneficiary_shares;
+    uint public m_expiryTime;
 
     /**
     * @dev Throw an exception if called before the expiry time
@@ -52,7 +60,8 @@ contract TimeBasedWill is Ownable {
     {
         //require(address(this).balance > 0, "Please add some initial funds to the Will");
         require(expiryDuration >= 1, "Expiry time should be atleast 1 sec");
-        m_expiryTime = now.add(expiryDuration);
+        m_expiryTime = expiryDuration.add(now);
+        m_num_beneficiaries = 0;
     }
 
     /**
@@ -61,55 +70,86 @@ contract TimeBasedWill is Ownable {
     */
     function changeExpiry(uint expiryDuration) onlyOwner beforeExpiry public
     {
-        m_expiryTime = now.add(expiryDuration);
+        m_expiryTime = expiryDuration.add(now);
     }
 
     /**
-    * @dev Adds the list of addresses as beneficiaries
-    * @notice Throws an exception if onwer is added as benefeciary
+    * @dev Adds the list of addresses as beneficiaries along with their individual shares
+    * @notice Throws an exception if 
+    *   - owner is added as beneficiary
+    *   - Num of beneficiary > 255
+    *   - Total shares dont add to 100
     */
-    function approveAddresses(address[] beneficiaries) public
-    {
+    function addBeneficiariesWithPercentShares(address[] beneficiaries, uint8[] shares) public onlyOwner beforeExpiry
+    {   
+        require(beneficiaries.length < 256, "Can only add upto 255 beneficiaries");
+        require(beneficiaries.length == shares.length, "Benefeciary and shares array length to be same");
+        uint8 total_shares = 0;
         for (uint i = 0; i < beneficiaries.length; i++) {
-            addBeneficiary(beneficiaries[i]);
+            require(beneficiaries[i] != m_owner, "Cannot add owner as beneficiary");
+            require(beneficiaries[i] != address(0), "Enter a valid address");
+            require(shares[i] > 0 && shares[i] <= 100, "Share has to > 0 & <= 100");
+            total_shares.add(shares[i]);
+        }
+        require(total_shares == 100, "Total shares should add to 100%");
+        _addBeneficiariesWithPercentShares(beneficiaries, shares);
+    }
+
+    function _addBeneficiariesWithPercentShares(address[] beneficiaries, uint8[] shares) internal
+    {   
+        // Will override the previous added beneficiaries
+        // Instead of clearing the previous array, if we override then its takes less gas
+        m_num_beneficiaries = 0;
+        uint8 min_length = 0;
+        if (m_beneficiaries.length <= beneficiaries.length) {
+            min_length = m_beneficiaries.length;
+        }
+        else {
+            min_length = beneficiaries.length;
+        }
+
+        // For min length, override the current elements
+        uint8 index = 0;
+        for (index; index < min_length; index++) {
+            delete m_beneficiary_shares[m_beneficiaries[index]];
+            m_beneficiaries[index] = beneficiaries[index];
+            m_beneficiary_shares[m_beneficiaries[index]] = Share(shares[index], false);
+        }
+        // If more elements added than previously present, push the new elements to the array
+        // and add them to the map
+        if (min_length == m_beneficiaries.length) {
+            for (index; index < beneficiaries.length; index++) {
+                m_beneficiaries.push(beneficiaries[index]);
+                m_beneficiary_shares[m_beneficiaries[index]] = Share(shares[index], false);                
+            }
+        }
+        // If lesser elements added than previously present, remove the extra elements from the array
+        // and delete them from the map
+        else {
+            for (uint8 i = m_beneficiaries.length-1; i >= index; i--) {
+                address beneficiary = m_beneficiaries.pop();
+                delete m_beneficiary_shares[beneficiary];
+            }
+            m_beneficiaries.length = min_length;
         }
     }
 
-    // As of now, we can enter the same benefeciary multiple times
-    function addBeneficiary(address newBeneficiary) public onlyOwner beforeExpiry
-    {
-        require(newBeneficiary != m_owner, "Cannot add owner as beneficiary");
-        require(newBeneficiary != address(0), "Enter a valid address");
-        m_beneficiaries.push(newBeneficiary);
-    }
-
     /**
-    * @dev Removes the benefeciary from the list of approved ones
-    * @notice Throws an exception if benefeciary was not added originally
-    */
-    function removeBeneficiary(address beneficiary) public onlyOwner beforeExpiry
-    {
-        require(m_beneficiaries.removeValue(beneficiary), "Address Not Found");
-    }
-
-    /**
-    * @dev Anyone can call this function after Expiry and funds will be transferred
-    * from the contract to the benefeciaries
+    * @dev Beneficiaries can call this after expiry to claim their ownership
+    * @notice Once the transfer is done, subsequent calls will throw an error
     */
     function claimOwnership() public payable afterExpiry
-    {
-        uint num_beneficiaries = m_beneficiaries.length;
-        require(num_beneficiaries > 0, "Add some beneficiaries to distribute");
-        require(address(this).balance >= num_beneficiaries * 1 wei, "Balance should be atleast num_beneficiaries * 1 wei");
-        emit benefeciary_address(m_beneficiaries[1], m_beneficiaries[1].balance);
-        uint shareOfAddress = address(this).balance.div(num_beneficiaries);
-        
+    {   
+        address payee = msg.sender;
+        Share payee_share = m_beneficiary_shares[payee];
 
-        for (uint i = 0; i < num_beneficiaries; i++) {
-            address value = m_beneficiaries[i];
-            value.transfer(shareOfAddress);
-        }
-        emit benefeciary_address(m_beneficiaries[1], m_beneficiaries[1].balance);
+        require(payee_share.m_percent_share > 0, "Address not present in the list of beneficiaries or no shares allocated");
+        require(payee_share.m_claimed_ownership == false, "Address has already claimed owenrship");
+
+        uint value = address(this).balance.mul(payee_share).div(100);
+        require(value != 0);
+        payee.transfer(value);
+        emit transferred_value(payee, value);
     }
 
     /**
